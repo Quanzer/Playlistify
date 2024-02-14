70e87b393592184845cf3c52dd60dbb69f18041e
Hong Liang Zhao <66213415+jiemazhb@users.noreply.github.com>
2024-02-13 16:51:58 -0800

    2/13 by Danny

--- Plalistify/firebase_database/src/App.js
+++ Plalistify/firebase_database/src/App.js
@@ -1,20 +1,10 @@
-import React from "react";
-import EmailList from "./EmailList";
-import Form from "./comp/form";
+import Login from "./Login/login"
+import Dashboard from "./dashboard"
 
-const App = () => {
-    return (
-        
-        <>
-        <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-        <img style={{transition:'transform .2s', width: 100, height: 100}}
-                            src="OIG.jpeg" />
-        <h2 style={{ color: "blue", fontWeight: "1000", fontSize: 100 * 0.0167+ 'vw' }}>Playlistify</h2>
-        </div>
-        <Form /> 
-        <EmailList />
-        </>
-    )
+const code = new URLSearchParams(window.location.search).get("code")
+
+function App(){
+    return code ? <Dashboard code = {code}/> : <Login/>        
 }
 
 export default App
--- Plalistify/firebase_database/src/index.js
+++ Plalistify/firebase_database/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './app'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//ReactDOM.render(<MyfExample></MyfExample>, document.getElementById("root"))
+const root = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 使用 createRoot 创建根 Root 对象
+const rootContainer = ReactDOM.createRoot(root);
+
+// 渲染组件
+rootContainer.render(<App />);
