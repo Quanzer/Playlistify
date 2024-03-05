3a2c4190d38f1c52d0ee64f4121bca0c2cf9a7d5
RandyMondragon <80940496+RandyMondragon@users.noreply.github.com>
2024-03-05 10:47:48 -0800

    Add files via upload
    Displays the audio features of tracks that are returned from search function

--- Plalistify/firebase_database/src/TrackSearchResult.js
+++ Plalistify/firebase_database/src/TrackSearchResult.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-export default function TrackSearchResult({track, chooseTrack}) {
-
-    function handlePlay(){
-        chooseTrack(track)
+export default function TrackSearchResult({ track, chooseTrack }) {
+    function handlePlay() {
+        chooseTrack(track);
     }
 
-  return (
-    <div className='d-flex m-2 align-items-center' style={{cursor: "pointer"}} onClick={handlePlay}>
-        <img src={track.albumUrl} style = {{height: "64px", width: "64px"}} />
-        <div className='ml-3'>
-            <div>{track.title}</div>
-            <div className='text-muted'>{track.artist}</div>
+    return (
+        <div className="d-flex m-2 align-items-center" style={{ cursor: 'pointer' }} onClick={handlePlay}>
+            <img src={track.albumUrl} style={{ height: '64px', width: '64px' }} alt="Album Art" />
+            <div className="ml-3">
+                <div>{track.title}</div>
+                <div className="text-muted">{track.artist}</div>
+                {track.audioFeatures && (
+                    <div>
+                        <p>Acousticness: {track.audioFeatures.acousticness}</p>
+                        <p>Tempo: {track.audioFeatures.tempo}</p>
+                        <p>Danceability: {track.audioFeatures.danceability}</p>
+                        <p>Energy: {track.audioFeatures.energy}</p>
+                        <p>Loudness: {track.audioFeatures.loudness}</p>
+                    </div>
+                )}
+            </div>
         </div>
-    </div>
-  )
+    );
 }
