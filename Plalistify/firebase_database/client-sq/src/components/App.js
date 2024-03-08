dd7de7d98b0578a77bd73b0bde4752f0c6a79ee1
Quanzer <quanforcollege@gmail.com>
2024-03-08 15:37:03 -0800

    history start and Routing
    routing and history

--- Plalistify/firebase_database/client-sq/src/components/App.js
+++ Plalistify/firebase_database/client-sq/src/components/App.js
@@ -2,12 +2,48 @@ import '../styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from './Dashboard.js';
 import Login from './Login';
+import io from 'socket.io-client';
+import Admin from './Admin.js';
+import History from "./History"
+import React, { useState, useEffect, useContext, useMemo } from "react";
+import { createContext } from 'react'
+import { Routes, Route } from "react-router-dom"
 
 const code = new URLSearchParams(window.location.search).get('code')
 
+export const SocketContext = createContext(io(process.env.REACT_APP_API_URL));
 function App() {
-  return code ? <Dashboard code ={code}/> : <Login/> 
+  const apiSocket = useContext(SocketContext);
+  useEffect(() => {
+
+    // Event Handlers
+    apiSocket.on('id', (res) => {
+      console.log('ID: ', res);
+    });
+
+    return () => {
+      apiSocket.off('id');
+      apiSocket.disconnect();
+    };
+  }, [])
+  return (
+    <>
+      <SocketContext.Provider value={apiSocket}>
+    
+        <Routes>
+            <Route path="/admin" element={<Admin />}>
+            </Route>
+            <Route path="/" element={<Dashboard/>}>
+            </Route>
+            <Route path="/history" element={<History />}>
+            </Route>
+          </Routes>
+     
+          </SocketContext.Provider>
+          </>
+  )
 
 }
+
 
 export default App;
